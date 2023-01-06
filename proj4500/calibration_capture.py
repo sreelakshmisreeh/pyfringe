8e4f61af2ea8e205797f1a8821bb97a1bfe45856
kl001 <kl001@adws.udayton.edu>
2023-01-05 19:19:04 -0500

    Update calibration_capture.py

--- proj4500/calibration_capture.py
+++ proj4500/calibration_capture.py
@@ -6,17 +6,23 @@ Created on Wed Jan  4 13:00:37 2023
 """
 
 import numpy as np
-import cv2
-import matplotlib.pyplot as plt
 import os
 import sys
 sys.path.append(r'C:\Users\kl001\pyfringe\proj4500')
 sys.path.append(r'C:\Users\kl001\Documents\pyfringe_test')
-import FringeAcquisition as gspy
+import gspy
 import proj4500
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 
-def calibration_capture(savedir, image_index_list, no_calib_images, proj_exposure_period = 27084, proj_frame_period = 33334):
+def calibration_capture(savedir, 
+                        image_index_list, 
+                        pattern_num_list, 
+                        no_calib_images, 
+                        proj_exposure_period, 
+                        proj_frame_period,
+                        do_insert_black=True,
+                        preview_image_index = 22,
+                        pprint_proj_status = True):
     
     cam_triggerType = "hardware"
     result, system, cam_list, num_cameras = gspy.sysScan()
@@ -25,7 +31,16 @@ def calibration_capture(savedir, image_index_list, no_calib_images, proj_exposur
         gspy.clearDir(savedir)
         cam = cam_list[0]
         for j in range(no_calib_images):
-            result &= proj4500.run_proj_single_camera(cam, savedir, j, cam_triggerType, image_index_list, proj_exposure_period, proj_frame_period )
+            result &= proj4500.run_proj_single_camera(cam, 
+                                                      savedir, j, 
+                                                      cam_triggerType, 
+                                                      image_index_list, 
+                                                      pattern_num_list,
+                                                      proj_exposure_period, 
+                                                      proj_frame_period,
+                                                      do_insert_black,
+                                                      preview_image_index,
+                                                      pprint_proj_status)
         print('Camera capture complete...')
         # Release reference to camera
         # NOTE: Unlike the C++ examples, we cannot rely on pointer objects being automatically
@@ -45,5 +60,12 @@ def calibration_capture(savedir, image_index_list, no_calib_images, proj_exposur
 #%%
 savedir = r'C:\Users\kl001\Documents\pyfringe_test\multifreq_calib_images'
 image_index_list =  np.repeat(np.arange(23,35),3).tolist()
-no_calib_images = 10
-calibration_capture(savedir, image_index_list, no_calib_images)
+pattern_num_list = [0,1,2] * len(set(image_index_list))
+proj_exposure_period = 27084; proj_frame_period = 33334
+no_calib_images = 2
+result = calibration_capture(savedir, 
+                             image_index_list, 
+                             pattern_num_list, 
+                             no_calib_images,
+                             proj_exposure_period, 
+                             proj_frame_period)
