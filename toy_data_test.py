49b7100e41229c730d07e4027784085045fd09f2
kl001 <kl001@adws.udayton.edu>
2023-01-31 11:10:42 -0500

    Update toy_data_test.py

--- toy_data_test.py
+++ toy_data_test.py
@@ -9,6 +9,7 @@ import numpy as np
 import cupy as cp
 import nstep_fringe as nstep
 import nstep_fringe_cp as nstep_cp
+import pickle
 
 
 def toy_generate():
@@ -41,49 +42,73 @@ def toy_generate():
 def main():
     test_limit = 0.9
     pitch_list = [50, 20]
-    N_list = [3, 3]
     fringe_arr_np = toy_generate()
     fringe_arr_cp = cp.asarray(fringe_arr_np)
-    delta_deck_np = nstep.delta_deck_gen(N_list[0], height=fringe_arr_np.shape[1], width=fringe_arr_np.shape[2])
-    delta_deck_cp = nstep_cp.delta_deck_gen_cp(N_list[0], height=fringe_arr_cp.shape[1], width=fringe_arr_cp.shape[2])
-    if delta_deck_np.all() == cp.asnumpy(delta_deck_cp).all():
-        print('\n Delta deck generation match')
-        masked_img_np_v1, modulation_np_v1, average_int_np_v1, phase_map_np_v1 = nstep.phase_cal(fringe_arr_np[0:3], delta_deck_np, test_limit)
-        masked_img_cp_v1, modulation_cp_v1, average_int_cp_v1, phase_map_cp_v1 = nstep_cp.phase_cal_cp(fringe_arr_cp[0:3], delta_deck_cp, test_limit)
-        masked_img_np_v2, modulation_np_v2, average_int_np_v2, phase_map_np_v2 = nstep.phase_cal(fringe_arr_np[6:9], delta_deck_np, test_limit)
-        masked_img_cp_v2, modulation_cp_v2, average_int_cp_v2, phase_map_cp_v2 = nstep_cp.phase_cal_cp(fringe_arr_cp[6:9], delta_deck_cp, test_limit)
-        
-        if (phase_map_np_v1.all() == cp.asnumpy(phase_map_cp_v1).all()) & (phase_map_np_v2.all() == cp.asnumpy(phase_map_cp_v2).all()):
-            print('\n All vertical phase maps match')
-            phase_arr_np = [phase_map_np_v1, phase_map_np_v2]
-            phase_arr_cp = [phase_map_cp_v1, phase_map_cp_v2]
-            multifreq_unwrap_np_v, k_arr_np_v = nstep.multifreq_unwrap(pitch_list, phase_arr_np, 1, 'v')
-            multifreq_unwrap_cp_v, k_arr_cp_v = nstep_cp.multifreq_unwrap_cp(pitch_list, phase_arr_cp, 1, 'v')
-            if multifreq_unwrap_np_v.all() == cp.asnumpy(multifreq_unwrap_cp_v).all():
-                print('\n Vertical unwrapped phase maps match')
-            else:
-                print('\n Vertical unwrapped phase map mismatch ')  
+    modulation_np_v1, white_img_np_v1, sin_lst_np_v1, cos_lst_np_v1, mask_np_v1 = nstep.phase_cal(fringe_arr_np[0:3], test_limit)
+    modulation_cp_v1, white_img_cp_v1, sin_lst_cp_v1, cos_lst_cp_v1, mask_cp_v1 = nstep_cp.phase_cal_cp(fringe_arr_cp[0:3], test_limit)
+    modulation_np_v2, white_img_np_v2, sin_lst_np_v2, cos_lst_np_v2, mask_np_v2 = nstep.phase_cal(fringe_arr_np[6:9], test_limit)
+    modulation_cp_v2, white_img_cp_v2, sin_lst_cp_v2, cos_lst_cp_v2, mask_cp_v2 = nstep_cp.phase_cal_cp(fringe_arr_cp[6:9], test_limit)
+    mask_np_v = mask_np_v1 & mask_np_v2
+    flag_np_v = np.where(mask_np_v == True)
+    sin_lst_np_v = np.array([sin_lst_np_v1[flag_np_v], sin_lst_np_v2[flag_np_v]])
+    cos_lst_np_v = np.array([cos_lst_np_v1[flag_np_v], cos_lst_np_v2[flag_np_v]])
+    phase_np_v = -np.arctan2(sin_lst_np_v, cos_lst_np_v)
+    mask_cp_v = mask_cp_v1 & mask_cp_v2
+    flag_cp_v = cp.where(mask_cp_v == True)
+    sin_lst_cp_v = cp.array([sin_lst_cp_v1[flag_cp_v], sin_lst_cp_v2[flag_cp_v]])
+    cos_lst_cp_v = cp.array([cos_lst_cp_v1[flag_cp_v], cos_lst_cp_v2[flag_cp_v]])
+    phase_cp_v = -cp.arctan2(sin_lst_cp_v, cos_lst_cp_v)
+    if (phase_np_v.all() == cp.asnumpy(phase_cp_v).all()):
+        print('\n All vertical phase maps match')
+        multifreq_unwrap_np_v, k_arr_np_v = nstep.multifreq_unwrap(pitch_list, phase_np_v, 1, 'v')
+        multifreq_unwrap_cp_v, k_arr_cp_v = nstep_cp.multifreq_unwrap_cp(pitch_list, phase_cp_v, 1, 'v')
+        if multifreq_unwrap_np_v.all() == cp.asnumpy(multifreq_unwrap_cp_v).all():
+            print('\n Vertical unwrapped phase maps match')
+            vertical_fringes_np = {"phase_map_np_v": phase_np_v,
+                                   "multifreq_unwrap_np_v": multifreq_unwrap_np_v}
+            vertical_fringes_cp = {"phase_map_cp_v": phase_cp_v,
+                                   "multifreq_unwrap_cp_v": multifreq_unwrap_cp_v}
+            with open(r'test_data\vertical_fringes_np.pickle', 'wb') as f:
+                pickle.dump(vertical_fringes_np, f)
+            with open(r'test_data\vertical_fringes_cp.pickle', 'wb') as f:
+                pickle.dump(vertical_fringes_cp, f)
         else:
-            print('\n Vertical phase map mismatch')
-        masked_img_np_h1, modulation_np_h1, average_int_np_h1, phase_map_np_h1 = nstep.phase_cal(fringe_arr_np[3:6], delta_deck_np, test_limit)
-        masked_img_cp_h1, modulation_cp_h1, average_int_cp_h1, phase_map_cp_h1 = nstep_cp.phase_cal_cp(fringe_arr_cp[3:6], delta_deck_cp, test_limit)
-        masked_img_np_h2, modulation_np_h2, average_int_np_h2, phase_map_np_h2 = nstep.phase_cal(fringe_arr_np[9:12], delta_deck_np, test_limit)
-        masked_img_cp_h2, modulation_cp_h2, average_int_cp_h2, phase_map_cp_h2 = nstep_cp.phase_cal_cp(fringe_arr_cp[9:12], delta_deck_cp, test_limit)
-        
-        if (phase_map_np_h1.all() == cp.asnumpy(phase_map_cp_h1).all()) & (phase_map_np_h2.all() == cp.asnumpy(phase_map_cp_h2).all()):
-            print('\n All horizontal phase maps match')
-            phase_arr_np = [phase_map_np_h1, phase_map_np_h2]
-            phase_arr_cp = [phase_map_cp_h1, phase_map_cp_h2]
-            multifreq_unwrap_np_h, k_arr_np_h = nstep.multifreq_unwrap(pitch_list, phase_arr_np, 1, 'h')
-            multifreq_unwrap_cp_h, k_arr_cp_h = nstep_cp.multifreq_unwrap_cp(pitch_list, phase_arr_cp, 1, 'h')
-            if multifreq_unwrap_np_h.all() == cp.asnumpy(multifreq_unwrap_cp_h).all():
-                print('\n Horizontal unwrapped phase maps match')
-            else:
-                print('\n Horizontal unwrapped phase map mismatch ')  
-        else:
-            print('\n Horizontal phase map mismatch')
+            print('\n Vertical unwrapped phase map mismatch ')  
     else:
-        print('Numpy and cupy delta deck arrays does not match')
+        print('\n Vertical phase map mismatch')
+    modulation_np_h1, white_img_np_h1, sin_lst_np_h1, cos_lst_np_h1, mask_np_h1 = nstep.phase_cal(fringe_arr_np[3:6], test_limit)
+    modulation_cp_h1, white_img_cp_h1, sin_lst_cp_h1, cos_lst_cp_h1, mask_cp_h1 = nstep_cp.phase_cal_cp(fringe_arr_cp[3:6], test_limit)
+    modulation_np_h2, white_img_np_h2, sin_lst_np_h2, cos_lst_np_h2, mask_np_h2 = nstep.phase_cal(fringe_arr_np[9:12], test_limit)
+    modulation_cp_h2, white_img_cp_h2, sin_lst_cp_h2, cos_lst_cp_h2, mask_cp_h2 = nstep_cp.phase_cal_cp(fringe_arr_cp[9:12], test_limit)
+    mask_np_h = mask_np_h1 & mask_np_h2
+    flag_np_h = np.where(mask_np_h == True)
+    sin_lst_np_h = np.array([sin_lst_np_h1[flag_np_h], sin_lst_np_h2[flag_np_h]])
+    cos_lst_np_h = np.array([cos_lst_np_h1[flag_np_h], cos_lst_np_h2[flag_np_h]])
+    phase_np_h = -np.arctan2(sin_lst_np_h, cos_lst_np_h)
+    mask_cp_h = mask_cp_h1 & mask_cp_h2
+    flag_cp_h = cp.where(mask_cp_h == True)
+    sin_lst_cp_h = cp.array([sin_lst_cp_h1[flag_cp_h], sin_lst_cp_h2[flag_cp_h]])
+    cos_lst_cp_h = cp.array([cos_lst_cp_h1[flag_cp_h], cos_lst_cp_h2[flag_cp_h]])
+    phase_cp_h = -cp.arctan2(sin_lst_cp_h, cos_lst_cp_h)
+    if (phase_np_h.all() == cp.asnumpy(phase_cp_h).all()):
+        print('\n All horizontal phase maps match')
+        multifreq_unwrap_np_h, k_arr_np_h = nstep.multifreq_unwrap(pitch_list, phase_np_h, 1, 'h')
+        multifreq_unwrap_cp_h, k_arr_cp_h = nstep_cp.multifreq_unwrap_cp(pitch_list, phase_cp_h, 1, 'h')
+        if multifreq_unwrap_np_h.all() == cp.asnumpy(multifreq_unwrap_cp_h).all():
+            print('\n Horizontal unwrapped phase maps match')
+            horizontal_fringes_np = {"phase_map_np_h": phase_np_h,
+                                     "multifreq_unwrap_np_h": multifreq_unwrap_np_h}
+            horizontal_fringes_cp = {"phase_map_cp_h": phase_cp_h,
+                                     "multifreq_unwrap_cp_h": multifreq_unwrap_cp_v}
+            with open(r'test_data\horizontal_fringes_np.pickle', 'wb') as f:
+                pickle.dump(horizontal_fringes_np, f)
+            with open(r'test_data\horizontal_fringes_cp.pickle', 'wb') as f:
+                pickle.dump(horizontal_fringes_cp, f)
+        else:
+            print('\n Horizontal unwrapped phase map mismatch ')  
+    else:
+        print('\n Horizontal phase map mismatch')
+
     return
 
 
