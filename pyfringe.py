18e35d6f1eda404fe3c8ad7ad21cb6dfa314e647
kl001 <kl001@adws.udayton.edu>
2023-02-12 16:23:04 -0500

    Update pyfringe.py

--- pyfringe.py
+++ pyfringe.py
@@ -10,28 +10,67 @@ import image_acquisation as acq
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+import image_acquisation as acq
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True" # needed when openCV and matplotlib used at the same time
 
-#%%
-#Gamma curve
-savedir = r'C:\Users\kl001\Documents\pyfringe_test\gamma_images'
-gamma_image_index_list = np.repeat(np.arange(5,22),3).tolist()
-gamma_pattern_num_list = [0,1,2] * len(set(gamma_image_index_list))
-result = acq.gamm_curve(gamma_image_index_list,gamma_pattern_num_list, savedir)
-#%%
-#Calibration
-savedir = r'C:\Users\kl001\Documents\pyfringe_test\multifreq_calib_images'
-image_index_list =  np.repeat(np.arange(22,34),3).tolist()
+#This is for checking running modulation experiment. This is new projector firmware with ground truth.
+#Ground truth: 
+#gt_pitch_list =[1000, 110, 16] 
+# gt_N_list = [16, 16, 16]
+
+savedir = r'C:\Users\kl001\Documents\pyfringe_test\modulation\ground_truth'
+image_index_list = np.repeat(np.arange(0,16),3).tolist()
 pattern_num_list = [0,1,2] * len(set(image_index_list))
-number_scan = 1
-acquisition_index = 0
-result = acq.calib_capture(image_index_list,pattern_num_list,savedir,number_scan, acquisition_index)
+savedir = r'C:\Users\kl001\Documents\grasshopper3_python\images'
+result = acq.run_proj_single_camera(savedir=savedir,
+                                     preview_option='Once',
+                                     number_scan=1,
+                                     acquisition_index=0,
+                                     image_index_list=image_index_list,
+                                     pattern_num_list=pattern_num_list,
+                                     cam_gain=0,
+                                     cam_bufferCount=15,
+                                     cam_capt_timeout=10,
+                                     cam_black_level=0,
+                                     cam_ExposureCompensation=0,
+                                     proj_exposure_period=27500,#Check image aquisation option2 for recomended value
+                                     proj_frame_period=34000,#
+                                     do_insert_black=True,
+                                     led_select=4,
+                                     preview_image_index=21,
+                                     focus_image_index=34,
+                                     image_section_size=None,
+                                     pprint_status=True,
+                                     save_npy=False,
+                                     save_jpeg=True)
+
 #%%
-#Mean pixel std(std)
-savedir = r'C:\Users\kl001\Documents\pyfringe_test\mean_pixel_std'
-acquisition_index = 10
-image_index = 13 #intensity = 200
-pattern_no = 0
-no_images = 50
-mean_std_pixel, std_pixel = acq.mranpixel_std(savedir, image_index, pattern_no, no_images, acquisition_index)
-#%%
+#This is for varying frequencies from
+#pitch_list = [1000,912,228,131,92,71,57,48,42,37,33,30,27,25,23,22,20,19,18,17,16]
+proj_width = 912
+proj_height = 1140
+savedir = r'C:\Users\kl001\Documents\pyfringe_test\modulation\allfreq'
+mod_image_index_list = np.repeat(np.arange(16,37),3).tolist()
+mod_pattern_num_list = [0,1,2] * len(set(image_index_list))
+savedir = r'C:\Users\kl001\Documents\grasshopper3_python\images'
+result = acq.run_proj_single_camera(savedir=savedir,
+                                     preview_option='Once',
+                                     number_scan=1,
+                                     acquisition_index=0,
+                                     image_index_list=mod_image_index_list,
+                                     pattern_num_list=mod_pattern_num_list,
+                                     cam_gain=0,
+                                     cam_bufferCount=15,
+                                     cam_capt_timeout=10,
+                                     cam_black_level=0,
+                                     cam_ExposureCompensation=0,
+                                     proj_exposure_period=27500,#Check image aquisation option2 for recomended value it will be diffrent since there are more images
+                                     proj_frame_period=34000,#
+                                     do_insert_black=True,
+                                     led_select=4,
+                                     preview_image_index=21,
+                                     focus_image_index=34,
+                                     image_section_size=None,
+                                     pprint_status=True,
+                                     save_npy=False,
+                                     save_jpeg=True)
